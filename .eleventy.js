6a1fc740097fa338f256cc3bb9a11c8d04f803f2
derekYankie <derek.afriyie@gmail.com>
2022-12-21 18:08:21 -0500

    add blog post template 🪄

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,20 @@
+// Format Date
+const { DateTime } = require("luxon");
+
 // Sources Files
 module.exports = function(eleventyConfig) {
     // Pass CSS and Images to public folder
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
 
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    });
+
     return {
         dir: {
         input: "src", // Grab the files I'm working out of (blog folder)
         output: "public" // Set output to public folder
     }
-    }
-};
+    };
+}
